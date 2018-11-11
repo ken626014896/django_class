b97c68abb3d74d430af4a75bf1c394454c039040
ken <626014896@qq.com>
2018-11-11 14:15:12 +0800

    视图函数

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from  . import views
 
 
 urlpatterns = [
 
     path('', views.index,name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+
 ]
