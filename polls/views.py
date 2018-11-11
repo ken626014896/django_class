b504bceba223ee51b589b82dda4507bb6d88e2a6
ken <626014896@qq.com>
2018-11-11 15:08:07 +0800

    表单form

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import  loader
+from django.urls import  reverse
+
+
 
 from polls.models import Question, Choice
 # Create your views here.
@@ -25,8 +28,22 @@ def detail(request, question_id):
     return render(request,"polls/detail.html" ,{"question":question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 发生choice未找到异常时，重新返回表单页面，并给出提示信息
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功处理数据后，自动跳转到结果页面，防止用户连续多次提交。
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
